c113c22a6e1dd07c972984714f875802e2559ba7
pereirafrederic <pereira.frederic.developpeur@gmail.com>
2019-06-30 17:48:06 +0200

    2 files modified

--- Activites/Profile/Profile.tsx
+++ Activites/Profile/Profile.tsx
@@ -1,9 +1,11 @@
 import React, { memo } from 'react'
 
+import { DiReact, DiAngularSimple, DiJava, DiMongodb ,FaDocker} from "react-icons/di";
 
 interface IProfile {
   id: number
   name: string
+  code: string
   version: string
   pratiquePro: string
   pratiquePerso: string
@@ -14,11 +16,39 @@ interface IProfile {
 interface IProps {
   profile: IProfile
 }
+const logoSize= 25;
 
 export default memo(({ profile }: IProps) => {
   console.log("MEMO Profiles RENDERING");
 
+  const switchLogo = () => {
+    console.log(profile.code)
+    switch (profile.code) {
+      case "java":
+        return <DiJava size={logoSize} />;
+        break
+              case "react":
+        return <DiReact size={logoSize} />;
+        break
+              case "angular":
+              case "angularJs":
+        return <DiAngularSimple size={logoSize} />;
+        break
+              case "mongo":
+        return <DiMongodb size={logoSize} />;
+        break
+              case "docker":
+        return <FaDocker size={logoSize} />;
+        break
+              case "react":
+        return <DiReact size={logoSize} />;
+        break
 
+      default:
+         return <DiReact size={logoSize} />;
+    }
+    return <DiReact size={logoSize} />;
+  }
 
   return (
     <div className="Profile" key={profile.id}>
@@ -26,13 +56,14 @@ export default memo(({ profile }: IProps) => {
 
       <div className="card">
         <div className="card-image">
-          <figure className="image is-4by3">
+          <figure className="image is-2by4">
+          {this.switchLogo()}
           </figure>
         </div>
         <div className="card-content">
           <div className="media">
             <div className="media-left">
-              <figure className="image is-48x48">
+              <figure className="image is-2by4">
               </figure>
             </div>
             <div className="media-content">
